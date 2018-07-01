71d949173df9d7fa40f9482b9ac0d3d83f0c5eac
Bruno <bruno.pejac@gmail.com>
2018-07-01 16:40:01 +0200

    remove redux logger

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { applyMiddleware, createStore, compose } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 import reducer from './reducers';
 import Mastermind from './Mastermind';
 
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(
-  applyMiddleware(
-    logger // neat middleware that logs actions
-  )
-));
+const store = createStore(
+  reducer,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
